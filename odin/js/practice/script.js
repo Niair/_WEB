f858f9d539608ca0086c7b27e067384f043a57a9
Niair <niair2180@gmail.com>
2026-03-23 21:16:48 +0530

    js practice

---
+++ odin/js/practice/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Nihal how are you")
+var name = "Nihal"
+console.log("hi " + name + " bro")
+let num1 = 5;
+let num2 = 8;
+console.log("Sum ", num1 + num2, " ok")
+num3 = 6;
+num4 = 7;
+console.log(num3 + num4);
+const pi = 3.14;
+console.log("The value of pi is : ", pi);
+num4 = 13;
+console.log("change the value of num4 to 13 and pi to 314", num4)
